abb9d3db8a65deb33a2fbde95ce6ff6b501c7eff
quad-Drills <90243053+quad-Drills@users.noreply.github.com>
2021-10-25 02:24:24 +0000

    typo challanger

--- commands/funsvell/fight.js
+++ commands/funsvell/fight.js
@@ -1,11 +1,13 @@
 module.exports=[{
  name:"fight",
  code: `
- $if[$getVar[challenger1]!="none"]
+ $if[$getVar[challenger1]!=none]
  Someone is already registered $getVar[challenger1]
  $if[$getVar[challenger1]==$userTag[$authorId]]
  You cancelled the registration
- $setVar[challenger1;"none"]
+ $elseif[$getVar[challenger1]!=$userTag[$authorId]]
+
+ $setVar[challenger1;none]
  $endif
  $else
  You register yourself $setVar[challenger1;$userTag[$authorId]]
@@ -21,5 +23,10 @@ module.exports=[{
   code:`
   $author[You set yourself as the variable]
   $setVar[challenger1;$userTag[$authorId]]
+  `
+},{
+  name:"clearin",
+  code:`
+  $setVar[challenger1;none]
   `
 }]
